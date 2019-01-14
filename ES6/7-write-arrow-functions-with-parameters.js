8f3b880c7ee3775c959d406646da1c18a5d2eb82
johnpospisil <audiofreak7@gmail.com>
2019-01-13 19:14:06 -0800

    Finish arrow Function lessons

---
+++ ES6/7-write-arrow-functions-with-parameters.js
@@ -0,0 +1,12 @@
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so // that the function uses arrow function syntax.
+
+// var myConcat = function(arr1, arr2) {
+//   "use strict";
+//   return arr1.concat(arr2);
+// };
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
+// returns 1,2,3,4,5
