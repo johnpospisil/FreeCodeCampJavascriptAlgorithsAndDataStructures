6bac80ce0924d70d1fea164b14be63ac3799f9bb
johnpospisil <audiofreak7@gmail.com>
2019-01-14 15:10:13 -0800

    Finish E6 lessons and exercises

---
+++ ES6/24-use--to-import-everything-from-a-file.js
@@ -0,0 +1,6 @@
+// The code below requires the contents of a file, "capitalize_strings", found in the 
+// same directory as it, imported. Add the appropriate import * statement to the top 
+// of the file, using the object provided.
+
+import * as stringsFile from "capitalize_strings";
+"use strict";
---
+++ ES6/26-import-a-default-export.js
@@ -0,0 +1,6 @@
+// In the following code, please import the default export, subtract, 
+// from the file "math_functions", found in the same directory as this file.
+
+"use strict";
+import subtract from "math_functions"
+subtract(7,4);
